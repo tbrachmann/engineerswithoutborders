e86bd469c822163721cbea988bbbd42041237d4c
Carmen Tang <carmentang@berkeley.edu>
2016-03-01 09:30:59 +0000

    turning off email notifications travis

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  # resources :cats
-  # root 'cats#index'
+  resources :cats
+  root 'cats#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
