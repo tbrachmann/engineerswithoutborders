240795487e5ddb5cb530f9b93489fa7ea96caa70
dandesonjew <danieltanders@gmail.com>
2016-03-29 23:18:13 +0000

    got first few steps of sad path + started working on pull ddown menu for states

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,3 +1,8 @@
 class Volunteer < ActiveRecord::Base
-    #validates :first_name, carmen => true    
+    #validates :first_name, carmen => true
+    def self.all_states
+        %w(CA AK AL AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY
+        LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK
+        OR PA RI SC SD TN TX UT VT VA WA WV WI WY)
+    end
 end
