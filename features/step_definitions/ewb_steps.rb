22d29bef1fa208caa5caca45b5d0694d021186e9
Jio Calderon <jpc1195@gmail.com>
2016-04-17 06:32:15 +0000

    finished fixing filter. double filter works.

--- features/step_definitions/ewb_steps.rb
+++ features/step_definitions/ewb_steps.rb
@@ -4,6 +4,18 @@ Given /the following volunteers exist/ do |volunteers_table|
   end
 end
 
+# Add a declarative step here for populating the DB with movies.
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    Movie.create(movie)
+    #is it ok to use movie.create here? will it parse movie correctly?
+    #do i need to save
+  end
+  # fail "Unimplemented"
+end
+
 Given /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
     all_ratings = Volunteer.all_ratings
     ratings = rating_list.split(", ")
