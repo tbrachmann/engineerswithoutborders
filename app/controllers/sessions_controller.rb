b2dbfbd3aa883bee72bac5744c95dad821390520
lawrencetvo <lawrencetvo@gmail.com>
2016-04-17 03:20:13 +0000

    fixed flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      redirect_to '/volunteers'
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
