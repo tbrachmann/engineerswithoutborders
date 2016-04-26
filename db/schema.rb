b49b2e48967ecdc5c3fd919253bad2dc6c12fd77
Carmen Tang <carmentang@berkeley.edu>
2016-04-26 20:25:26 +0000

    See all users when first one volunteer page

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160424024952) do
-=======
 ActiveRecord::Schema.define(version: 20160426041810) do
->>>>>>> e154c7554f795ea33f3a5de6fe7dfe4fec04fb72
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
