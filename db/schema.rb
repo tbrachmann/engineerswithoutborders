29b3ee10cf7ee038625c548cac5b41f849bbe73b
lawrencetvo <lawrencetvo@gmail.com>
2016-04-26 04:37:48 +0000

    added remember me functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405035116) do
+ActiveRecord::Schema.define(version: 20160426041810) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160405035116) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   create_table "volunteers", force: :cascade do |t|
