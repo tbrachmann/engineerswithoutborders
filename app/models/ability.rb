a013e9e8367164ed20e7493a531bcfcfeac48a0d
Jack Chen <yuxi.chen@berkeley.edu>
2016-10-27 08:05:33 +0000

    Working on CanCanCan features

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -30,7 +30,7 @@ class Ability
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
      user ||= User.new # guest user (not logged in)
-      if user.admin?
+      if user.admin? #user admin?
         can :manage, :all
       else
         can :read, :all
