f0c0549f7b5b94f51260a66d49349b0f67a283c1
Jack Chen <yuxi.chen@berkeley.edu>
2016-11-25 18:15:44 +0000

    implemented younger than 18 age for profile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,19 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  # Change mail delivery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port:587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["yuxi.chen1994"],
+    password: ENV["2wmohammad1m"],
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
