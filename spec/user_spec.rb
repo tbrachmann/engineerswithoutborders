881a1666af1aa555f9f09267944151060ae208e3
Jack Chen <yuxi.chen@berkeley.edu>
2016-12-02 06:11:46 +0000

    pushing up more user rspec tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -50,3 +50,53 @@ RSpec.describe User, "#education" do
     end
 end
 
+RSpec.describe User, "#school" do 
+    context "when the user first enters in their school name" do 
+        it "will save the school name into the school field" do
+            user = User.new
+            user.school = "UC Berkeley"
+            expect(user.school).to eq "UC Berkeley"
+        end
+    end
+end
+
+RSpec.describe User, "#expertise" do 
+    context "when the user first enters in their expertise status" do 
+        it "will save their expertise into the expertise field" do
+            user = User.new
+            user.expertise = "Civil Engineering"
+            expect(user.expertise).to eq "Civil Engineering"
+        end
+    end
+end
+
+RSpec.describe User, "#certifications" do 
+    context "when the user first enters in their certifications" do 
+        it "will save their certifications into the certificartions field" do
+            user = User.new
+            user.certifications = "Chemical"
+            expect(user.certifications).to eq "Chemical"
+        end
+    end
+end
+
+RSpec.describe User, "#time_commitment" do 
+    context "when the user first enters in their time commitment" do 
+        it "will save their time commitment into the time commitment field" do
+            user = User.new
+            user.time_commitment = "1-3 hours every month"
+            expect(user.time_commitment).to eq "1-3 hours every month"
+        end
+    end
+end
+
+RSpec.describe User, "#travel" do 
+    context "when the user first enters in their travel ability" do 
+        it "will save their travel ability into the travel field" do
+            user = User.new
+            user.travel = "Yes"
+            expect(user.travel).to eq "Yes"
+        end
+    end
+end
+
