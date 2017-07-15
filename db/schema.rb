fbadd869c0f6a1c961c4e43aabd92d58e196967e
Claire Scheele <cscheele@berkeley.edu>
2017-07-15 21:30:47 +0000

    Deleted events view folder, controller, model. Deleted project model dependencies on events. Deleted events table from schema.rb. Deleted events code from application view and welcome view.

--- db/schema.rb
+++ db/schema.rb
@@ -13,19 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20170712234605) do
 
-  create_table "events", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "name"
-    t.string   "description"
-    t.date     "start_time"
-    t.date     "end_time"
-    t.string   "location"
-    t.boolean  "repeats"
-    t.string   "days"
-    t.integer  "project_id"
-  end
-
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
