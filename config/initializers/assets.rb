32860adcc4a6811e33c259b189be80387c58e125
Arman <armanjaffer@gmail.com>
2017-07-17 07:37:17 +0000

    add more precompile exemptions

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
