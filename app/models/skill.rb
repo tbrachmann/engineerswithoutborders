a99b70e8c1412d363ee860aca835289f6f77a6c0
Claire Scheele <cscheele@berkeley.edu>
2017-07-23 18:20:23 +0000

    Fixed conflict in skill.rb

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,7 +1,6 @@
 class Skill < ActiveRecord::Base
     has_and_belongs_to_many :users
     has_and_belongs_to_many :projects
-    
     def self.get_skills(skill_ids)
         total = []
         if skill_ids
