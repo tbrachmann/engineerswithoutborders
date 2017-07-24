795243c7e5dfb8febfa10e8dfba34813af7177dd
Arman <armanjaffer@gmail.com>
2017-07-24 17:55:34 +0000

    Fix: Modifying Migrations to fix a heroku bug that's blocking migrations

--- db/migrate/20170708020315_add_manager_to_projects.rb
+++ db/migrate/20170708020315_add_manager_to_projects.rb
@@ -1,5 +1,4 @@
 class AddManagerToProjects < ActiveRecord::Migration
   def change
-    add_foreign_key :projects, :users
   end
 end
--- db/migrate/20170708020714_add_manager_to_projects_fix.rb
+++ db/migrate/20170708020714_add_manager_to_projects_fix.rb
@@ -1,6 +1,5 @@
 class AddManagerToProjectsFix < ActiveRecord::Migration
   def change
-    remove_foreign_key :projects, :users
     add_column :projects, :manager_id, :integer
   end
 end
