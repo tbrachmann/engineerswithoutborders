310b07fb9736de783382ced7e972ade1acee5fcd
Tobias Brachmann <tobybrachmann@gmail.com>
2017-07-24 15:24:52 -0700

    (feat) Fake data for demo purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Leaving these unchanged, but I think its better to generate test data using
+# Cucumber and step definitions for isolation purposes.
+
 user_list = [["jbarns@berkeley.edu", "password", "Jack", "Barnett", 20],
              ["mikewazoski@gmail.com", "password", "Mike", "Wazoski", 131],
              ["natalie@gmail.com", "password", "Natalie", "Barnett", 17],
              ["benbarns@gmail.com", "password", "Ben", "Barnett", 14]]
 
-user_list.each do |email, password, first_name, last_name, age|
+user_list.map! do |email, password, first_name, last_name, age|
   User.create(email: email, password: password, first_name: first_name,
               last_name: last_name, age: age)
 end
 
-skill_list = ["test1", "test2", "test3"]
+skills = [Faker::Job.key_skill, Faker::Job.key_skill, Faker::Job.key_skill]
 
-skill_list.each do |name|
-    Skill.create(name: name)
+skills.map! do |skill|
+    Skill.create(name: skill)
+end
+
+skills.each do |skill|
+  user_num = Faker::Number.between(0, 3)
+  user_list[user_num].skills << skill
 end
   
 test_manager = User.create(first_name: Faker::Name::first_name, 
@@ -51,3 +59,24 @@ test_project = Project.create(name: "Remba Island Project",
 test_project.managers << test_manager
 test_project.managers << test_manager_two
 test_project.managers << test_manager_three
+
+# Will generate some fake data for Adolfo
+
+User.create(first_name: "Tobias", last_name: "Brachmann",
+            email: "tobybrachmann@gmail.com", password: "asdfgh",
+            manager: true, admin: true)
+
+adolfo = User.create(first_name: "Adolfo", last_name: "Espino",
+                     email: "adolfo.espino@gmail.com", password: "asdfgh")
+test_project = FactoryGirl.create(:project)
+adolfo.manager = true
+adolfo.manages << test_project
+
+5.times do
+  FactoryGirl.create(:project)
+end
+
+10.times do
+  FactoryGirl.create(:user, password: "asdfgh")
+end
+
--- spec/factories/project_factory.rb
+++ spec/factories/project_factory.rb
@@ -2,5 +2,12 @@ FactoryGirl.define do
     factory :project do
         created_at { 5.years.ago }
         updated_at { 10.years.ago }
+        start_date { 2.days.ago}
+        name { Faker::Movie.quote }
+        description { Faker::Company.bs }
+        volunteer_capacity { Faker::Number.number(2) }
+        location { Faker::Address.city }
+        #image { Faker::LoremPixel.image("50x50") }
+        hours_per_week { Faker::Number.number(2) }
     end
 end
