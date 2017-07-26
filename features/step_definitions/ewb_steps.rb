0a1c7071f38fd0be07ddee835da95fef18266fa3
Claire Scheele <cscheele@berkeley.edu>
2017-07-26 05:01:45 +0000

    [A[A[B[B[B'maint': Added puts arg1 in pending step definitions so Code Climate wouldn't complain about unused arguments.

--- features/step_definitions/ewb_steps.rb
+++ features/step_definitions/ewb_steps.rb
@@ -225,15 +225,18 @@ Then(/^I should see that the page theme and colors are consistent with the main 
 end
 
 Given(/^the following users exist with the given qualities:$/) do |table|
+  puts table
   # table is a Cucumber::MultilineArgument::DataTable
   pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I should see "([^"]*)"'s qualities$/) do |arg1|
+  puts arg1
   pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I should not see "([^"]*)"'s qualities$/) do |arg1|
+  puts arg1
   pending # Write code here that turns the phrase above into concrete actions
 end
 
