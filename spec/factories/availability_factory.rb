e25a008d88ad64d02dfe3832f51d37a32d9921c2
Arman <armanjaffer@gmail.com>
2017-07-29 08:40:17 +0000

    Maint: added tests for availability.to_s

---
+++ spec/factories/availability_factory.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :availability do
+    monday_morning false
+    monday_afternoon true
+    monday_evening true
+    tuesday_morning false
+    tuesday_afternoon false
+    thursday_afternoon true
+  end
+end
