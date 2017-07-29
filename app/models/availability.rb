7d8cb56f293fb872a00ef25033b302602ead73d1
Arman <armanjaffer@gmail.com>
2017-07-29 07:55:28 +0000

    Feat: Human readable availability rather than object

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -6,4 +6,26 @@ class Availability < ActiveRecord::Base
     # this simply makes it easier to access params[:user_params[:param]]
         params.require(:user)
     end 
+    
+    def symbol_to_readable sym
+        lst = sym.to_s.split("_")
+        return_str = []
+        lst.each do |li|
+            if li.length > 0
+                return_str << li.capitalize
+            end
+        end
+        return_str.join(" ")
+    end
+    
+    def to_s
+        avail = []
+        self.attributes.each do |x, y|
+            if y && !!y == y
+                avail << x
+            end
+        end
+        avail.map{|k| (symbol_to_readable(k))}.join(", ")
+    end
+    
 end
