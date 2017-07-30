5885284bc39d27061284de8775fec4530392b10d
Arman <armanjaffer@gmail.com>
2017-07-30 01:45:14 +0000

    quick: halfway through adding tests

---
+++ spec/application_helper_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+include ApplicationHelper
+RSpec.describe ApplicationHelper, "#helper" do
+  context "when full_title is called without an argument" do
+    it "formats the page title with the base title" do
+        expect(full_title()).to eq("Engineers Without Borders")
+    end
+  end
+end
+
+RSpec.describe ApplicationHelper, "#helper" do
+  context "when full_title is called with an argument" do
+    it "formats the page title with the base title" do
+        expect(full_title("Good Page Name")).to eq("Good Page Name | Engineers Without Borders")
+    end
+  end
+end
+
+RSpec.describe ApplicationHelper, "#helper" do
+  context "when sortable is called" do
+    it "lets see what it does" do
+        
+    end
+  end
+end
+
