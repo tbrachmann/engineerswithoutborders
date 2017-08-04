f3354cdda452ff2d6172f044d37929b927539813
Bill Yeo <yeo.dongh@berkeley.edu>
2017-08-04 23:52:02 +0000

    added navbar-toggle

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,16 +2,10 @@ class Role < ActiveRecord::Base
   has_many :users
   
   def self.get_role(role_ids)
-      total = []
-      if role_ids
-          role_ids.each do |x|
-              if Role.exists? x
-                  total << Role.find(x)
-              end
-          end
-          return total
+      if Role.exists? x
+          Role.find_id(x)
       else
-          return []
+          return nil
       end
   end
   
