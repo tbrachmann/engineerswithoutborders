4cea1ee8263590a9790b35c500c3bbe6ac34c9aa
Bill Yeo <yeo.dongh@berkeley.edu>
2017-08-04 17:52:44 +0000

    add select_role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,20 @@
 class Role < ActiveRecord::Base
   has_many :users
-
+  
+  def self.get_role(role_ids)
+      total = []
+      if role_ids
+          role_ids.each do |x|
+              if Role.exists? x
+                  total << Role.find(x)
+              end
+          end
+          return total
+      else
+          return []
+      end
+  end
+  
   def self.ransackable_attributes(auth_object = nil)
     ['name']
   end
