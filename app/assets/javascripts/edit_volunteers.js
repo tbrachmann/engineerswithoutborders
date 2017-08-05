6ede9464071d67fa1520840b3ff347e03be8cd5b
Tobias Brachmann <tobybrachmann@gmail.com>
2017-08-04 18:44:24 -0700

    (feat) Added searchabilit to add volunteers to project

--- app/assets/javascripts/edit_volunteers.js
+++ app/assets/javascripts/edit_volunteers.js
@@ -1,12 +1,48 @@
 var EditVolunteers = {
     init: function() {
 	//Initialize the multi-select
-	$('#my-select:hidden').multiSelect({afterInit: function(){
-	    $('center#edit_volunteers:hidden').show()
-	}})
+	$('#my-select:hidden').multiSelect({
+	    selectableHeader:"<input type='text' class='search-input' autocomplete='off' placeholder='Search'>",
+	    selectionHeader:"<input type='text' class='search-input' autocomplete='off' placeholder='Search'>",
+	    selectableFooter:"<div class='custom-header'>Eligible Volunteers</div>",
+	    selectionFooter:"<div class='custom-header'>Your Volunteers</div>",
+	    afterInit: function(){
+		$('center#edit_volunteers:hidden').show()
+		var that = this,
+		    $selectableSearch = that.$selectableUl.prev(),
+		    $selectionSearch = that.$selectionUl.prev(),
+		    selectableSearchString = '#'+that.$container.attr('id')+' .ms-elem-selectable:not(.ms-selected)',
+		    selectionSearchString = '#'+that.$container.attr('id')+' .ms-elem-selection.ms-selected';
+
+		that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
+		    .on('keydown', function(e){
+			if (e.which === 40){
+			    that.$selectableUl.focus();
+			    return false;
+			}
+		    });
+
+		that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
+		    .on('keydown', function(e){
+			if (e.which == 40){
+			    that.$selectionUl.focus();
+			    return false;
+			}
+		    })
+	    },
+	    afterSelect: function(){
+		this.qs1.cache();
+		this.qs2.cache();
+	    },
+	    afterDeselect: function(){
+		this.qs1.cache();
+		this.qs2.cache();
+	    }
+	})
 	//Bind event to button
 	$("#submit").click(function(e) {
-	    EditVolunteers.sendSelected(EditVolunteers.getSelectedIds())
+	    var selected = EditVolunteers.getSelectedIds()
+	    EditVolunteers.sendSelected(selected)
 	})
     },
     getSelected: function() {
@@ -39,6 +75,6 @@ var EditVolunteers = {
     }
 }
 
-$(document).on("turbolinks:load ready", function() {
+$(document).on("turbolinks:load", function() {
     EditVolunteers.init()
 })
